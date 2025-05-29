2706cb1c9337f26706fe1ed39221a6aa946d4d8c
a8anassis <a8anassis@gmail.com>
2025-05-29 18:41:40 +0300

    fix: clear

--- src/main/java/gr/aueb/cf/teacherapp/repository/TeacherRepository.java
+++ src/main/java/gr/aueb/cf/teacherapp/repository/TeacherRepository.java
@@ -3,6 +3,7 @@ package gr.aueb.cf.teacherapp.repository;
 import gr.aueb.cf.teacherapp.model.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
